0c81d4718121041569239c47eb7115a8c3e0a263
Don-Cryptus <don.cryptus@gmail.com>
2023-06-04 23:11:56 +0200

    clean up

--- src-tauri/src/connection.rs
+++ src-tauri/src/connection.rs
@@ -1,9 +1,9 @@
-use dotenv::dotenv;
+
 use migration::{DbErr, Migrator, MigratorTrait};
 use sea_orm::{Database, DbConn};
 
 pub async fn establish_connection() -> Result<DbConn, DbErr> {
-    dotenv().ok();
+
     // let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
     let database_url = String::from("sqlite://clippy.sqlite?mode=rwc");
     println!("Connecting to {}", database_url);
