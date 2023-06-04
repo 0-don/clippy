b21a49b489c99018f623433e5ae97c3594aef7ea
Don-Cryptus <don.cryptus@gmail.com>
2023-06-04 20:39:37 +0200

    add db support

---
+++ src-tauri/src/connection.rs
@@ -0,0 +1,17 @@
+use dotenv::dotenv;
+use migration::{DbErr, Migrator, MigratorTrait};
+use sea_orm::{Database, DbConn};
+
+pub async fn establish_connection() -> Result<DbConn, DbErr> {
+    dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+    println!("Connecting to {}", database_url);
+    let db = Database::connect(&database_url)
+        .await
+        .expect("Failed to setup the database");
+    Migrator::up(&db, None)
+        .await
+        .expect("Failed to run migrations for tests");
+
+    Ok(db)
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use entity::post;
+
+use sea_orm::{ActiveModelTrait, Set};
+use sea_orm::{DatabaseConnection, DbErr};
+
+mod connection;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
+    tokio::spawn(async move { insert().await });
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
+async fn insert() -> Result<String, DbErr> {
+    let db: DatabaseConnection = connection::establish_connection().await?;
+
+    let post = post::ActiveModel {
+        title: Set(String::from("Amazing title 1")),
+        text: Set(String::from("Lorem ipsum dolor sit amet.")),
+        ..Default::default()
+    };
+
+    let post: post::Model = post.insert(&db).await?;
+
+    println!("Post created with ID: {}, TITLE: {}", post.id, post.title);
+
+    Ok(String::from("ok"))
+}
+
+#[tokio::main]
+async fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
