5f4ab00f7822a68de4b02858081b29d80aac85f7
Don-Cryptus <don.cryptus@gmail.com>
2023-06-11 19:02:49 +0200

    test

--- src-tauri/src/commands/clipboard.rs
+++ src-tauri/src/commands/clipboard.rs
@@ -1,34 +1,8 @@
-use entity::clipboard::{self, ActiveModel, Model};
-use sea_orm::{ActiveModelTrait, Set};
-use sea_orm::{DatabaseConnection, DbErr};
-
-use crate::connection;
+use entity::clipboard::{self};
 
 #[tauri::command]
-pub async fn insert_clipboard(clipboard: clipboard::Model) -> Result<Model, String> {
+pub async fn insert_clipboard(clipboard: clipboard::Model) -> Result<String, String> {
     println!("{:?}", clipboard);
-    let clip = insert(clipboard).await;
 
-    Ok(clip.unwrap())
-}
-
-async fn insert(clipboard: clipboard::Model) -> Result<Model, DbErr> {
-    let db: DatabaseConnection = connection::establish_connection().await?;
-
-    let model = ActiveModel {
-        r#type: Set(clipboard.r#type),
-        content: Set(clipboard.content),
-
-        star: Set(Some(false)),
-
-        blob: Set(clipboard.blob),
-        height: Set(clipboard.height),
-        width: Set(clipboard.width),
-        size: Set(clipboard.size),
-        ..Default::default()
-    };
-
-    let clip_db: clipboard::Model = model.insert(&db).await?;
-
-    Ok(clip_db)
+    Ok("clip.unwrap()".to_string())
 }
--- src-tauri/src/utils/clipboard.rs
+++ src-tauri/src/utils/clipboard.rs
@@ -1,35 +1,52 @@
 extern crate alloc;
 use alloc::borrow::Cow;
 use arboard::{Clipboard, ImageData};
+use async_trait::async_trait;
 use clipboard_master::{CallbackResult, ClipboardHandler};
+use entity::clipboard::{ActiveModel, Model};
+use sea_orm::{ActiveModelTrait, DatabaseConnection, DbErr, Set};
 use std::{io, process::Command};
+use tauri::regex::Regex;
+
+use crate::connection;
 
 pub struct Handler;
 
 impl ClipboardHandler for Handler {
     fn on_clipboard_change(&mut self) -> CallbackResult {
-        Command::new("clear").status().unwrap();
-        
-        let mut clipboard = Clipboard::new().unwrap();
+        let (text, image) = get_clipboard();
 
-        let text = match clipboard.get_text() {
-            Ok(text) => text,
-            Err(_) => "".to_string(),
+        let re = Regex::new(r"^#?(?:[0-9a-f]{3}){1,2}$").unwrap();
+
+        let r#type = if text.is_empty() {
+            Set("image".to_string())
+        } else if re.is_match(&text) {
+            Set("color".to_string())
+        } else {
+            Set("text".to_string())
         };
 
-        let image = match clipboard.get_image() {
-            Ok(image) => image,
-            Err(_) => ImageData {
-                width: 0,
-                height: 0,
-                bytes: Cow::from([0].as_ref()),
-            },
+        let img = if image.width > 0 {
+            ActiveModel {
+                blob: Set(Some(image.bytes.to_vec())),
+                height: Set(Some(image.height as i32)),
+                width: Set(Some(image.width as i32)),
+                size: Set(Some(image.bytes.to_vec().len().to_string())),
+                ..Default::default()
+            }
+        } else {
+            ActiveModel {
+                ..Default::default()
+            }
         };
 
-        println!("{}", text);
-        println!("{:?} {:?}", image.height, image.width);
+        let model = ActiveModel {
+            r#type,
+            content: Set(Some(text)),
 
-        let blob = image.bytes.into_owned();
+            star: Set(Some(false)),
+            ..img
+        };
 
         CallbackResult::Next
     }
@@ -38,4 +55,34 @@ impl ClipboardHandler for Handler {
         eprintln!("Error: {}", error);
         CallbackResult::Next
     }
+}
+
+fn get_clipboard() -> (String, ImageData<'static>) {
+    Command::new("clear").status().unwrap();
+
+    let mut clipboard = Clipboard::new().unwrap();
+
+    let text = match clipboard.get_text() {
+        Ok(text) => text,
+        Err(_) => "".to_string(),
+    };
+
+    let image = match clipboard.get_image() {
+        Ok(image) => image,
+        Err(_) => ImageData {
+            width: 0,
+            height: 0,
+            bytes: Cow::from([0].as_ref()),
+        },
+    };
+
+    (text, image)
+}
+
+async fn insert(clipboard: ActiveModel) -> Result<Model, DbErr> {
+    let db: DatabaseConnection = connection::establish_connection().await?;
+
+    let clip_db: Model = clipboard.insert(&db).await?;
+
+    Ok(clip_db)
 }
