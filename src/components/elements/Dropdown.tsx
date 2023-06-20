020828892a26b035db7f1d44fdb363410d72d1e8
Don-Cryptus <don.cryptus@gmail.com>
2023-06-20 23:17:48 +0200

    test

--- src/components/elements/Dropdown.tsx
+++ src/components/elements/Dropdown.tsx
@@ -33,8 +33,6 @@ export const Dropdown: Component<DropdownProps> = ({
       onOpenChange={onOpenChange}
       onChange={onChange}
       defaultValue={value}
-      multiple={false}
-      virtualized={true}
       itemComponent={(props) => (
         <Combobox.Item
           item={props.item}
@@ -42,7 +40,7 @@ export const Dropdown: Component<DropdownProps> = ({
             props.item.rawValue === value
               ? "bg-indigo-600 text-white"
               : "text-white"
-          } flex justify-between`}
+          } flex cursor-pointer items-center justify-between px-2 hover:bg-indigo-600`}
         >
           <Combobox.ItemLabel>{props.item.rawValue}</Combobox.ItemLabel>
           <Combobox.ItemIndicator>
@@ -53,15 +51,15 @@ export const Dropdown: Component<DropdownProps> = ({
     >
       <Combobox.Control class="rounded-md border border-gray-300 p-1 focus:outline-none dark:border-dark-light dark:bg-dark-light dark:text-white dark:focus:bg-dark-dark">
         <Combobox.Trigger class="flex items-center">
-          <Combobox.Input class="w-8 cursor-pointer bg-transparent outline-none text-center" />
-          <Combobox.Icon class="">
+          <Combobox.Input class="w-8 cursor-pointer bg-transparent text-center outline-none" />
+          <Combobox.Icon>
             <VsArrowSwap class="rotate-90" />
           </Combobox.Icon>
         </Combobox.Trigger>
       </Combobox.Control>
       <Combobox.Portal>
-        <Combobox.Content class="">
-          <Combobox.Listbox class="absolute z-10 max-h-24 w-full overflow-auto rounded-md bg-dark-light py-1 text-sm shadow-lg" />
+        <Combobox.Content>
+          <Combobox.Listbox class="absolute -top-1.5 z-10 max-h-24 w-full overflow-auto rounded-md bg-dark-light text-sm shadow-lg" />
         </Combobox.Content>
       </Combobox.Portal>
     </Combobox.Root>
