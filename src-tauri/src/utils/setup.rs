b79ede6666ff846ef3deacb3d058dce66b4140e9
don.cryptus <don.cryptus@gmail.com>
2023-10-03 21:19:50 +0200

    easy

--- src-tauri/src/utils/setup.rs
+++ src-tauri/src/utils/setup.rs
@@ -1,4 +1,4 @@
-use super::hotkey::hotkey_listener::init_hotkey_listener;
+use super::hotkey::hotkey_listener::{init_hotkey_listener, upsert_hotkeys_in_store};
 use crate::types::types::Key;
 use crate::{
     service::window::get_data_path, types::types::Config,
@@ -11,38 +11,53 @@ use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use std::{fs, path::Path, sync::OnceLock};
 use tauri::{LogicalSize, Manager};
+use tokio::sync::oneshot;
 // use window_shadows::set_shadow;
 
 pub static MAIN_WINDOW_X: i32 = 375;
 pub static MAIN_WINDOW_Y: i32 = 600;
 
 pub static APP: OnceLock<tauri::AppHandle> = OnceLock::new();
+
 pub static HOTKEY_MANAGER: OnceLock<GlobalHotKeyManager> = OnceLock::new();
 pub static HOTKEYS: OnceLock<Arc<Mutex<HashMap<u32, Key>>>> = OnceLock::new();
+pub static HOTKEY_STOP_TX: OnceLock<Mutex<Option<oneshot::Sender<()>>>> = OnceLock::new();
 pub static CLIPBOARD: OnceLock<Arc<Mutex<Clipboard>>> = OnceLock::new();
+
+pub static GLOBAL_EVENTS: [&'static str; 2] = ["window_display_toggle", "recent_clipboards"];
+
+pub static VIEW_MORE_EVENTS: [&'static str; 4] =
+    ["sync_clipboard_history", "preferences", "about", "exit"];
+
+pub static SIDEBAR_ICON_EVENTS: [&'static str; 4] = [
+    "recent_clipboards",
+    "starred_clipboards",
+    "history",
+    "view_more",
+];
 
 pub fn setup(app: &mut tauri::App) -> Result<(), Box<(dyn std::error::Error + 'static)>> {
     APP.set(app.handle()).expect("error initializing tauri app");
     let _ = HOTKEY_MANAGER.set(GlobalHotKeyManager::new().unwrap());
     let _ = HOTKEYS.set(Arc::new(Mutex::new(HashMap::new())));
     let _ = CLIPBOARD.set(Arc::new(Mutex::new(Clipboard::new()?)));
+    HOTKEY_STOP_TX.set(Mutex::new(None)).unwrap_or_else(|_| {
+        panic!("Failed to initialize HOTKEY_STOP_TX");
+    });
 
     create_config();
 
     let window = app.get_window("main").unwrap();
-
     let _ = window.set_size(LogicalSize::new(MAIN_WINDOW_X, MAIN_WINDOW_Y));
-
     #[cfg(any(windows, target_os = "macos"))]
     set_shadow(&window, true).unwrap();
-
     #[cfg(debug_assertions)]
     {
         window.open_devtools();
     }
 
     tauri::async_runtime::spawn(async { Master::new(Handler).run() });
-    // tauri::async_runtime::spawn(async { init_hotkey_listener() });
+    tauri::async_runtime::spawn(async { upsert_hotkeys_in_store().await });
 
     init_hotkey_listener();
 
