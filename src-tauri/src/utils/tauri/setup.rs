a1f6a43a00ed88f9e8001eb5d85b9356d5c05310
don.cryptus <don.cryptus@gmail.com>
2023-10-07 22:00:12 +0200

    easy

--- src-tauri/src/utils/tauri/setup.rs
+++ src-tauri/src/utils/tauri/setup.rs
@@ -1,5 +1,8 @@
 use super::config::{create_config, init_config, init_window};
-use crate::events::{clipboard_events::Handler, hotkey_events::init_hotkey_listener, window_events::window_event_listener};
+use crate::events::{
+    clipboard_events::Handler, hotkey_events::init_hotkey_listener,
+    window_events::window_event_listener,
+};
 use clipboard_master::Master;
 
 // use window_shadows::set_shadow;
@@ -8,8 +11,8 @@ pub static GLOBAL_EVENTS: [&'static str; 2] = ["window_display_toggle", "type_cl
 
 pub fn setup(app: &mut tauri::App) -> Result<(), Box<(dyn std::error::Error + 'static)>> {
     create_config();
-    init_config(app)?;
-    init_window(app)?;
+    init_config(app);
+    init_window(app);
 
     window_event_listener();
     tauri::async_runtime::spawn(async { Master::new(Handler).run() });
