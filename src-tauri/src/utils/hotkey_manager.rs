b2d8a23596a1faebc5fa3db5b4d68f5b7a374c5b
don.cryptus <don.cryptus@gmail.com>
2023-10-08 02:50:45 +0200

    easy

--- src-tauri/src/utils/hotkey_manager.rs
+++ src-tauri/src/utils/hotkey_manager.rs
@@ -1,17 +1,35 @@
+use std::{collections::HashMap, sync::MutexGuard};
+
 use super::tauri::config::{GLOBAL_EVENTS, HOTKEYS, HOTKEY_MANAGER};
 use crate::{service::hotkey::get_all_hotkeys_db, types::types::Key};
-use global_hotkey::hotkey::HotKey;
+use global_hotkey::{hotkey::HotKey, GlobalHotKeyManager};
+
+fn get_hotkeys_and_manager() -> (
+    MutexGuard<'static, HashMap<u32, Key>>,
+    &'static GlobalHotKeyManager,
+) {
+    let hotkeys_store = HOTKEYS
+        .get()
+        .expect("Failed to get HOTKEYS")
+        .lock()
+        .expect("Failed to lock HOTKEYS");
+    let hotkey_manager = HOTKEY_MANAGER.get().expect("Failed to get HOTKEY_MANAGER");
+    (hotkeys_store, hotkey_manager)
+}
 
 pub fn register_hotkeys(all: bool) {
     tauri::async_runtime::spawn(async move {
-        let hotkeys_store = HOTKEYS.get().unwrap().lock().unwrap();
-        let hotkey_manager = HOTKEY_MANAGER.get().unwrap();
-
+        let (hotkeys_store, hotkey_manager) = get_hotkeys_and_manager();
+        println!("{:?}", hotkeys_store);
         for (_, hotkey) in hotkeys_store.iter() {
             if all || hotkey.global {
                 if hotkey_manager.register(hotkey.hotkey).is_err() {
-                    hotkey_manager.unregister(hotkey.hotkey).unwrap();
-                    hotkey_manager.register(hotkey.hotkey).unwrap();
+                    hotkey_manager
+                        .unregister(hotkey.hotkey)
+                        .expect("Failed to unregister hotkey");
+                    hotkey_manager
+                        .register(hotkey.hotkey)
+                        .expect("Failed to register hotkey");
                 }
             }
         }
@@ -20,36 +38,45 @@ pub fn register_hotkeys(all: bool) {
 
 pub fn unregister_hotkeys(all: bool) {
     tauri::async_runtime::spawn(async move {
-        let hotkeys_store = HOTKEYS.get().unwrap().lock().unwrap();
-        let hotkey_manager = HOTKEY_MANAGER.get().unwrap();
-
+        let (hotkeys_store, hotkey_manager) = get_hotkeys_and_manager();
         for (_, hotkey) in hotkeys_store.iter() {
             if all || !hotkey.global {
-                hotkey_manager.unregister(hotkey.hotkey).unwrap();
+                hotkey_manager
+                    .unregister(hotkey.hotkey)
+                    .expect("Failed to unregister hotkey");
             }
         }
     });
 }
 
+fn insert_hotkey_into_store(key: Key) {
+    if HOTKEYS
+        .get()
+        .unwrap()
+        .lock()
+        .unwrap()
+        .get(&key.id)
+        .is_some()
+    {
+        let _ = HOTKEYS.get().unwrap().lock().unwrap().remove(&key.id);
+    }
+    HOTKEYS.get().unwrap().lock().unwrap().insert(key.id, key);
+}
+
 pub async fn upsert_hotkeys_in_store() -> Result<(), Box<dyn std::error::Error>> {
     let hotkeys = get_all_hotkeys_db().await?;
-    let mut hotkey_store = HOTKEYS.get().unwrap().lock().unwrap();
 
     for hotkey in hotkeys {
-        let hotkey_str: String = parse_shortcut(
+        let hotkey_str = parse_shortcut(
             hotkey.ctrl,
             hotkey.alt,
             hotkey.shift,
-            format!("Key{}", &hotkey.key.to_uppercase()).as_str(),
+            &format!("Key{}", &hotkey.key.to_uppercase()),
         );
-
         let key: HotKey = hotkey_str.parse()?;
-
-        let global = GLOBAL_EVENTS.contains(&hotkey.event.as_str());
-
-        let key = Key {
+        let key_struct = Key {
             id: key.id(),
-            global,
+            global: GLOBAL_EVENTS.contains(&hotkey.event.as_str()),
             event: hotkey.event,
             key_str: hotkey_str,
             ctrl: hotkey.ctrl,
@@ -58,85 +85,57 @@ pub async fn upsert_hotkeys_in_store() -> Result<(), Box<dyn std::error::Error>>
             key: hotkey.key,
             hotkey: key,
         };
-
-        if hotkey_store.get(&key.id).is_some() {
-            let _ = hotkey_store.remove(&key.id);
-        }
-
-        hotkey_store.insert(key.id, key);
+        insert_hotkey_into_store(key_struct);
     }
 
     // Add 1..9 regular keys which are not global
     for i in 1..=9 {
-        let hotkey_digit = parse_shortcut(
-            false,
-            false,
-            false,
-            format!("Digit{}", i.to_string()).to_uppercase().as_str(),
-        );
-
+        let hotkey_digit = parse_shortcut(false, false, false, &format!("Digit{}", i));
         let key_digit: HotKey = hotkey_digit.parse()?;
-
-        let key_digit_struct = Key {
-            id: key_digit.id(),
-            global: false,               // These keys are not global
-            event: format!("digis{}", i), // Adjust this if you have specific events for the keys
-            key_str: hotkey_digit,
-            ctrl: false,
-            alt: false,
-            shift: false,
-            key: i.to_string(),
-            hotkey: key_digit,
-        };
-
-        if hotkey_store.get(&key_digit_struct.id).is_some() {
-            let _ = hotkey_store.remove(&key_digit_struct.id);
-        }
-
-        hotkey_store.insert(key_digit_struct.id, key_digit_struct);
-
-        let hotkey_num = parse_shortcut(
-            false,
-            false,
-            false,
-            format!("Digit{}", i.to_string()).to_uppercase().as_str(),
-        );
-
+        let hotkey_num = parse_shortcut(false, false, false, &format!("Numpad{}", i));
         let key_num: HotKey = hotkey_num.parse()?;
 
-        let key_num_struct = Key {
-            id: key_num.id(),
-            global: false,               // These keys are not global
-            event: format!("key_{}", i), // Adjust this if you have specific events for the keys
-            key_str: hotkey_num,
-            ctrl: false,
-            alt: false,
-            shift: false,
-            key: i.to_string(),
-            hotkey: key_num,
-        };
-
-        if hotkey_store.get(&key_num_struct.id).is_some() {
-            let _ = hotkey_store.remove(&key_num_struct.id);
+        let key_structs = vec![
+            Key {
+                id: key_digit.id(),
+                global: false,
+                event: format!("digit_{}", i),
+                key_str: hotkey_digit.clone(),
+                ctrl: false,
+                alt: false,
+                shift: false,
+                key: i.to_string(),
+                hotkey: key_digit,
+            },
+            Key {
+                id: key_num.id(),
+                global: false,
+                event: format!("num_{}", i),
+                key_str: hotkey_num,
+                ctrl: false,
+                alt: false,
+                shift: false,
+                key: i.to_string(),
+                hotkey: key_num,
+            },
+        ];
+        for key_struct in key_structs {
+            insert_hotkey_into_store(key_struct);
         }
-
-        hotkey_store.insert(key_num_struct.id, key_num_struct);
     }
 
     Ok(())
 }
 
 pub fn parse_shortcut(ctrl: bool, alt: bool, shift: bool, key: &str) -> String {
-    let mut modifiers = Vec::new();
-    if ctrl {
-        modifiers.push("Control");
-    }
-    if alt {
-        modifiers.push("Alt");
-    }
-    if shift {
-        modifiers.push("Shift");
-    }
+    let modifiers: Vec<&str> = vec![
+        if ctrl { "Control" } else { "" },
+        if alt { "Alt" } else { "" },
+        if shift { "Shift" } else { "" },
+    ]
+    .into_iter()
+    .filter(|s| !s.is_empty())
+    .collect();
 
     format!(
         "{}{}{}",
