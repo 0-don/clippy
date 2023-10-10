90b6088a93f4133ce60c576fd17ad67e8e7442de
don.cryptus <don.cryptus@gmail.com>
2023-10-11 00:21:49 +0200

    easy

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import { listen } from "@tauri-apps/api/event";
 import { createResource, onMount } from "solid-js";
 import { render } from "solid-js/web";
+import { WindowName } from "./@types";
 import App from "./components/pages/app/App";
 import AppStore from "./store/AppStore";
 import HotkeyStore from "./store/HotkeyStore";
 import "./styles.css";
 import { TabId } from "./utils/constants";
+import { openWindow } from "./utils/helpers";
 
 const Index = () => {
   const { setGlobalHotkeyEvent } = HotkeyStore;
@@ -19,6 +21,10 @@ const Index = () => {
     );
 
     listen("init", init);
+
+    listen("open_window", ({ payload }: { payload: WindowName }) =>
+      openWindow(payload),
+    );
 
     listen("change_tab", ({ payload }: { payload: TabId }) =>
       setCurrentTab(payload),
