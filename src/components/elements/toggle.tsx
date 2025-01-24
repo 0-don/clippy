23b7450a529bd6000c1a09eee30bfc9eb8329d15
0-don <70039285+0-don@users.noreply.github.com>
2025-01-24 11:31:33 +0100

    add encrypt / decrypt password check

--- src/components/elements/toggle.tsx
+++ src/components/elements/toggle.tsx
@@ -1,6 +1,7 @@
 import { FiCheck } from "solid-icons/fi";
 import { VsClose } from "solid-icons/vs";
 import { Component } from "solid-js";
+import { cn } from "../../lib/utils";
 
 type ToggleProps = {
   checked?: boolean;
@@ -10,7 +11,7 @@ type ToggleProps = {
 
 export const Toggle: Component<ToggleProps> = (props) => {
   return (
-    <label class="relative inline-flex cursor-pointer items-center">
+    <label class="relative inline-flex items-center">
       <input
         type="checkbox"
         checked={props.checked}
@@ -19,9 +20,12 @@ export const Toggle: Component<ToggleProps> = (props) => {
         disabled={props.disabled}
       />
       <div
-        class={`peer relative h-4 w-11 rounded-full bg-gray-200 peer-checked:bg-indigo-600 peer-focus:outline-hidden after:absolute after:start-[2px] after:top-0 after:h-4 after:w-4 after:rounded-full after:border after:border-transparent after:bg-white after:transition-all after:content-[''] peer-checked:after:translate-x-[150%] peer-checked:after:border-transparent peer-checked:rtl:after:-translate-x-full dark:border-gray-600 ${
-          props.checked ? "dark:bg-indigo-700" : "dark:bg-dark-light"
-        } dark:bg-opacity-20 after:z-40 dark:after:bg-zinc-700`}
+        class={cn(
+          "relative h-4 w-11 rounded-full peer-focus:outline-hidden after:absolute after:start-[2px] after:top-0 after:h-4 after:w-4 after:rounded-full after:border after:border-transparent after:bg-white after:transition-all after:content-[''] peer-checked:after:translate-x-[150%] peer-checked:after:border-transparent peer-checked:rtl:after:-translate-x-full dark:border-gray-600",
+          "dark:bg-opacity-20 peer-checked:bg-indigo-60 bg-gray-200 after:z-40 dark:after:bg-zinc-700",
+          props.checked ? "dark:bg-indigo-700" : "dark:bg-dark-light",
+          props?.disabled ? "cursor-not-allowed" : "cursor-pointer"
+        )}
       >
         <div class="absolute inset-0 z-50 flex items-center justify-between px-1">
           {props.checked ? <FiCheck class="ml-auto text-sm text-white" /> : <VsClose class="text-white" />}
