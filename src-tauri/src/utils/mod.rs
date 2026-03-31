dbacdde3ab23746a29ba819ac9285ac6c13bd845
0-don <don.cryptus@gmail.com>
2026-04-01 00:23:07 +0200

    fix: enable optional OCR feature and adjust related configurations

--- src-tauri/src/utils/mod.rs
+++ src-tauri/src/utils/mod.rs
@@ -1,6 +1,7 @@
 pub mod clipboard_manager;
 pub mod fullscreen_detector;
 pub mod hotkey_manager;
+#[cfg(not(all(target_os = "windows", target_arch = "aarch64")))]
 pub mod ocr;
 pub mod providers;
 pub mod sync_manager;
